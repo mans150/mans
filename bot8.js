45ad89a0de7dbe26d293941d547b073eff632969
mans150 <45200060+mans150@users.noreply.github.com>
2019-02-22 11:47:20 +0200

    Update bot8.js

--- bot8.js
+++ bot8.js
@@ -4,6 +4,10 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "!"
  
 client.on('message', msg => {
   if (msg.content === 'ping') {
