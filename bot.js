4ad8c1be866ab4e6cbc4e6a9bc1dc0e433ae7907
mans150 <45200060+mans150@users.noreply.github.com>
2019-03-05 21:25:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1227,14 +1227,6 @@ client.on('message', msg => {
  
 
 
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
-const client = new Discord.Client();
 
 /*
 packages:
