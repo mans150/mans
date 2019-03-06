e89062cef32f29e033e84562b6ea36e840b10523
mans150 <45200060+mans150@users.noreply.github.com>
2019-03-06 13:58:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1463,25 +1463,8 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 }
  
 });
-client.on("message", message => {
-    if (message.content === `${prefix}help`) {
-  const embed = new Discord.RichEmbed()
-      .setColor("#000000")
-      .setDescription(`
-${prefix}play ⇏ لتشغيل أغنية برآبط أو بأسم
-${prefix}skip ⇏ لتجآوز الأغنية الحآلية
-${prefix}stop ⇏ إيقآف الأغنية مؤقتا
-${prefix}resume ⇏ لموآصلة الإغنية بعد إيقآفهآ مؤقتا
-${prefix}vol ⇏ لتغيير درجة الصوت 100 - 0
-${prefix}leave⇏ لإخرآج البوت من الروم
-${prefix}np ⇏ لمعرفة الأغنية المشغلة حآليا
-${prefix}queue ⇏ لمعرفة قآئمة التشغيل
- `)
-   message.channel.sendEmbed(embed)
-   
-   }
-   });
+
  
- 
+
  
 client.login(process.env.BOT_TOKEN);
