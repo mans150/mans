eb52ce1ef78a326a20551ddaef9ac54bc84e4bf3
mans150 <45200060+mans150@users.noreply.github.com>
2019-03-07 20:38:37 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -666,7 +666,7 @@ if (message.content.startsWith(prefix + 'clear')) {
 client.on('message', message => {
   if (true) {
 if (message.content === '!support') {
-      message.author.send(' |https://discord.gg/T4Q7vXP| لـ أي استفسار').catch(e => console.log(e.stack));
+      message.author.send(' |https://discord.gg/QEXgvw| لـ أي استفسار').catch(e => console.log(e.stack));
  
     }
    }
@@ -690,8 +690,8 @@ client.on('message', message => {
  
 client.on('message', message => {
   if (true) {
-if (message.content === '!invite') {
-      message.author.send(' رابط البوت |   ').catch(e => console.log(e.stack));
+if (message.content === '!inv') {
+      message.author.send(' رابط البوت | https://discordapp.com/api/oauth2/authorize?client_id=553282332484960279&permissions=8&scope=bot  ').catch(e => console.log(e.stack));
  
     }
    }
@@ -700,7 +700,7 @@ if (message.content === '!invite') {
  
  
 client.on('message', message => {
-     if (message.content === "!invite") {
+     if (message.content === "!inv") {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
